0cc043b74abcdff142f996c40656fcde8ad8fcb5
Tess <njerigitundu003@gmail.com>
2022-04-17 00:09:56 +0300

    Create repositories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
