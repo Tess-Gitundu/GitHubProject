a0574cad3d1559042ce6a12912197f5380f46755
Tess <njerigitundu003@gmail.com>
2022-04-19 00:15:06 +0300

    Add more information on profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  author:any;
+  
+  constructor( private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(`https://api.github.com/users/Tess-Gitundu`)
+    .subscribe(Response => {
+      console.log(Response);
+      this.author = Response;
+    })
   }
+
 
 }
