73a4bf31f4a4d82ead0181b4b8282f088c6c90c5
Tess <njerigitundu003@gmail.com>
2022-04-19 00:38:20 +0300

    Create route link to profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ReposComponent } from './repos/repos.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
+  { path: '', component: ProfileComponent},
   { path: 'users', component: UserComponent },
   { path: 'repos', component: ReposComponent }
 ];
